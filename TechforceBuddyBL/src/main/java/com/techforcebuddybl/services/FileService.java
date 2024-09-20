b119eff09c5c96f8295c24e244431336ed226670
unnatijajal <88263364+unnatijajal@users.noreply.github.com>
2024-09-20 10:33:52 +0530

    Delete TechforceBuddyBL/src/main/java/com/techforcebuddybl/services/FileService.java

--- TechforceBuddyBL/src/main/java/com/techforcebuddybl/services/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.techforcebuddybl.services;
-
-import com.techforcebuddybl.entity.FileEntity;
-
-public interface FileService {
-	public FileEntity findFileByFileName(String fileName);
-}
